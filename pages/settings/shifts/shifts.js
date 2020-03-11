9f272653da9c265b636edac1af7a87f85b2a0833
Ali TEK <alitek@gmail.com>
2020-03-11 22:05:12 +0300

    vardiya tanimlari eklendi. menu icon lar duzenlendi.

---
+++ pages/settings/shifts/shifts.js
@@ -0,0 +1,155 @@
+module.exports = function(req,res,callback){
+	var data={
+		form:{
+			name:'',
+			times:[],
+			passive:false
+		},
+		filter:{},
+		list:[]
+	}
+
+	if(!req.query.db){
+		return callback({code:'ACTIVE DB ERROR',message:'Aktif secili bir veri ambari yok.'});
+	}
+	switch(req.params.func || ''){
+		case 'addnew':
+		
+		addnew(req,res,data,callback);
+		break;
+		case 'edit':
+			edit(req,res,data,callback);
+		break;
+		case 'view':
+			view(req,res,data,callback);
+		break;
+		case 'delete':
+		
+		deleteItem(req,res,data,callback);
+		break;
+		default:
+			getList(req,res,data,callback);
+		break;
+	}
+	
+}
+
+function getList(req,res,data,callback){
+	if(req.method=='POST'){
+		var filter={};
+		
+		for(let k in req.body){
+			if(req.body[k] && k!='btnFilter'){
+				filter[k]=req.body[k];
+			}
+		}
+
+		res.redirect('/settings/shifts?db=' + req.query.db + '&' + mrutil.encodeUrl(filter) + '&sid=' + req.query.sid);
+	}else{
+		data.filter=Object.assign(data.filter,req.query);
+		
+		data.filter.db=undefined;
+		delete data.filter.db;
+		data.filter.sid=undefined;
+		delete data.filter.sid;
+
+		api.get('/' + req.query.db + '/shifts',req,data.filter,(err,resp)=>{
+			if(!err){
+				data=mrutil.setGridData(data,resp);
+				data.list.forEach((e)=>{
+					e['zaman']=zamanText(e);
+				})
+			}else{
+				errorLog(err);
+			}
+			
+			callback(null,data);
+		});
+		
+	}
+
+}
+
+function zamanText(doc){
+	if(!doc.times) return '';
+	var sbuf='';
+	doc.times.forEach((zaman,index)=>{
+		sbuf +=zaman.name + '(' + zaman.startHour.n2() + ':' + zaman.startMinute.n2() + ' - ' + zaman.endHour.n2() + ':' + zaman.endMinute.n2() + ')';
+		if(index<doc.times.length-1){
+			sbuf +=', ';
+		}
+	});
+
+	return sbuf;
+}
+
+function addnew(req,res,data,callback){
+	//data['title']='Yeni Lokasyon';
+	if(req.method=='POST'){
+		data.form=Object.assign(data.form,req.body);
+		api.post('/' + req.query.db + '/shifts',req,data.form,(err,resp)=>{
+			if(!err){
+				res.redirect('/settings/shifts?db=' + req.query.db +'&sid=' + req.query.sid);
+				return;
+				}else{
+					data['message']=err.message;
+					callback(null,data);
+				}
+			});
+	}else{
+		callback(null,data);
+	}
+}
+
+
+function edit(req,res,data,callback){
+	var _id=req.params.id || '';
+	if(req.method=='POST' || req.method=='PUT'){
+		data.form=Object.assign(data.form,req.body);
+		api.put('/' + req.query.db + '/shifts/' + _id,req,data.form,(err,resp)=>{
+			if(!err){
+				res.redirect('/settings/shifts?db=' + req.query.db +'&sid=' + req.query.sid);
+
+			}else{
+				data['message']=err.message;
+				callback(null,data);
+			}
+		});
+	}else{
+		api.get('/' + req.query.db + '/shifts/' + _id,req,null,(err,resp)=>{
+			if(!err){
+				data.form=Object.assign(data.form,resp.data);
+				callback(null,data);
+			}else{
+				data['message']=err.message;
+				callback(null,data);
+			}
+		});
+	}
+}
+
+function view(req,res,data,callback){
+	var _id=req.params.id || '';
+	api.get('/' + req.query.db + '/shifts/' + _id,req,null,(err,resp)=>{
+		if(!err){
+			data.form=Object.assign(data.form,resp.data);
+			callback(null,data);
+		}else{
+			data['message']=err.message;
+			callback(null,data);
+		}
+	});
+}
+
+function deleteItem(req,res,data,callback){
+	var _id=req.params.id || '';
+	api.delete('/' + req.query.db + '/shifts/' + _id,req,(err,resp)=>{
+		if(!err){
+			res.redirect('/settings/shifts?db=' + req.query.db +'&sid=' + req.query.sid);
+			
+		}else{
+			data['message']=err.message;
+			callback(null,data);
+		}
+	});
+}
